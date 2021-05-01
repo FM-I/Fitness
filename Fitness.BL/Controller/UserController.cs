7162f2cbe5e1972bd8d6211444129427cc3343ea
FM-I <ubrfmi@gmail.com>
2021-05-01 13:57:31 +0300

    Добавлен контролер пользователя. Создана начальная реализация загрузки и сохранения пользователя.

---
+++ Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Fitness.BL.Model;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Fitness.BL.Controller
+{
+    /// <summary>
+    /// Контролер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public void Save() 
+        {
+            var formattor = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formattor.Serialize(fs, User);
+            }
+        }
+        /// <summary>
+        /// Получть данные пользователя.
+        /// </summary>
+        /// <returns> Польлзователь. </returns>
+        public UserController()
+        {
+            var formattor = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if(formattor.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Создание контролера пользователя.
+        /// </summary>
+        /// <param name="userName"> Пользователь. </param>
+        public UserController(string userName, string genderName, DateTime birthDate, double weight, double height)
+        {
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDate, weight, height);
+        }
+    }
+}
--- Fitness.CMD/Program.cs
+++ Fitness.CMD/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Fitness.BL.Controller;
+using Fitness.BL.Model;
+using System;
 
 namespace Fitness.CMD
 {
@@ -6,6 +8,26 @@ namespace Fitness.CMD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Fitness");
+
+            Console.WriteLine("Введите имя пользователя");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения");
+            var birthDay = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите вес");
+            var weight = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост");
+            var height = Double.Parse(Console.ReadLine());
+
+
+            var userController = new UserController(name, gender, birthDay, weight, height);
+            userController.Save();
         }
     }
 }

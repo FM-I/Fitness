33056e622b85ef171a67865075d623f01bc280ce
FM-I <ubrfmi@gmail.com>
2021-05-01 13:17:40 +0300

    Создано модель пользователя.

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название пола.
+        /// </summary>
+        public string GenderName { get; }
+
+        /// <summary>
+        /// Создать новый гендер.
+        /// </summary>
+        /// <param name="genderName">Имя пола.</param>
+        public Gender(string genderName)
+        {
+            if(string.IsNullOrWhiteSpace(genderName))
+            {
+                throw new ArgumentNullException("Имя поля не может быть пустым или null", nameof(genderName));
+            }
+
+            GenderName = genderName;
+        }
+
+        public override string ToString()
+        {
+            return GenderName;
+        }
+
+    }
+}

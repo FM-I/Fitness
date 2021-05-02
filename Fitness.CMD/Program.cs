ffd5cf305a2e72640c4396e03030911ff7b3b4f6
FM-I <ubrfmi@gmail.com>
2021-05-02 18:40:16 +0300

    Добавлено локализацию

--- Fitness.CMD/Program.cs
+++ Fitness.CMD/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Fitness.BL.Controller;
 using Fitness.BL.Model;
 using System;
+using System.Globalization;
+using System.Resources;
 
 namespace Fitness.CMD
 {
@@ -8,9 +10,13 @@ namespace Fitness.CMD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Fitness");
 
-            Console.Write("Введите имя пользователя : ");
+            var culture = CultureInfo.CreateSpecificCulture("en-us");
+            var resourceManager = new ResourceManager("Fitness.CMD.Languages.Messages", typeof(Program).Assembly);
+
+
+            Console.WriteLine(resourceManager.GetString("Welcome", culture));
+            Console.Write(resourceManager.GetString("EnterName", culture));
             var name = Console.ReadLine();
 
             var userController = new UserController(name);
